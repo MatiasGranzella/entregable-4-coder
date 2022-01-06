dc51f39f91c220bc4d65dcdd5eb2f23f12c5145d
Matias Federico Granzella <65555836+MatiasGranzella@users.noreply.github.com>
2022-01-06 18:32:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const cont = require('./class_conteiner.js');
 const express = require("express");
 const { Router } = express;
 
-
 const app = express();
 const router = Router();
 let new_conteiner = new cont.Conteiner("./src/courses.txt");
@@ -21,24 +20,24 @@ router.get('/courses', (req, res) => {
 
 
 router.get('/courses/:id', (req, res) => {
-    let course_id = new_conteiner.getById(req.query.id)
+    let course_id = (req.query.id == null) ? req.params.id : req.query.id
+    let course_data = new_conteiner.getById(course_id)
     
-    if (course_id != null) {
-        res.json(course_id)
+    if (course_data != null) {
+        res.json(course_data)
     } else {
         res.json({error : 'Course not found'})
     }
 })
 
 router.post('/courses', (req, res) => {
-
-    res.json(new_conteiner.save(req.query))
+    res.json(new_conteiner.save(req.body))
     new_conteiner.writeFile()
 })
 
 router.put('/courses/:id', (req, res) => {
-    let modify_course_id = new_conteiner.modifyById(req.query.id, req.query)
-    console.log(req.query);
+    let modify_course_id = new_conteiner.modifyById(req.params.id, req.body)
+
     if (modify_course_id != null) {
         res.json(modify_course_id)
     } else {
@@ -49,8 +48,7 @@ router.put('/courses/:id', (req, res) => {
 })
 
 router.delete('/courses/:id', (req, res) => {
-    console.log(req)
-    let delete_course_id = new_conteiner.deleteById(req.query.id)
+    let delete_course_id = new_conteiner.deleteById(req.params.id)
 
     if (delete_course_id != null) {
         res.json(delete_course_id)
